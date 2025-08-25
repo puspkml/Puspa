964d83beeb224017697bd595da727d6fe5022791
puspkml <puspakamal7214@gmail.com>
2025-08-25 12:06:20 +0530

    Update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,10 @@
   <!-- Main -->
   <main class="flex flex-col justify-center items-center text-center px-6">
     <h2 class="text-4xl md:text-5xl font-serif font-bold mb-4">
-      Creator & <span class="text-gray-600">Curious Mind</span>
+      <span class="text-zinc-600">Physicist</span>,Creator & <span class="text-gray-600">Curious Mind</span>
     </h2>
     <p class="text-lg italic text-gray-500 max-w-2xl leading-relaxed">
-      Passionate about building interactive experiences, writing thoughtful blogs, and exploring creative possibilities.  
+      Passionate about building interactive experiences, writing thoughts, and exploring creative possibilities.  
       This is where my projects, games, and ideas come to life.
     </p>
   </main>
