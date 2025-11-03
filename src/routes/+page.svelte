6441bef3de2b0962915939d1b3f3e30de2d6ee85
Puspa Kamal Rai <132550374+puspkml@users.noreply.github.com>
2025-11-03 09:33:26 +0530

    Revise personal description and meta tags in page.svelte
    Updated the meta description and content to reflect new interests and projects. Enhanced personal introduction and clarified professional background.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <svelte:head>
   <title>Puspa Kamal Rai</title>
-  <meta name="description" content="Offical website of Puspa Kamal Rai — Physicist, Creator, and Curious Mind. Explore my blog, projects, and games." />
+  <meta name="description" content="I'm a Flutemoligist, who loves programming and developing web applications. Driven by a deep passion for Physics, I combine it with programming to turn my ideas and imagination into reality." />
 </svelte:head>
 
 <div class="h-screen flex flex-col bg-gradient-to-b from-gray-50 to-white text-gray-800 font-sans overflow-hidden">
@@ -12,11 +12,14 @@
   <!-- Main -->
   <main class="flex flex-col justify-center items-center text-center px-6">
     <h2 class="text-4xl md:text-5xl font-serif font-bold mb-4">
-      <span class="text-zinc-600">Physicist</span>,Creator & <span class="text-gray-600">Curious Mind</span>
+      <span class="text-zinc-600">Physicronics</span>,Codelian & <span class="text-gray-600">Musicologist</span>
     </h2>
     <p class="text-lg italic text-gray-500 max-w-2xl leading-relaxed">
-      Passionate about building interactive experiences, writing thoughts, and exploring creative possibilities.  
-      This is where my projects, games, and ideas come to life.
+      I’m Puspa Kamal Rai, a Physics undergraduate at Sri Sathya Sai Institute of Higher Learning, passionate about research, design, and technology-driven innovation. My interests span semiconductors, optics, embedded systems, and automation, where I love turning theoretical concepts into real-world solutions.
+
+I’ve developed projects like a 3D-printed smartphone spectrometer at IIT Kharagpur, a wireless automated bell system, and a custom UPS, combining creativity with technical precision. Skilled in Python, Svelte Kit, Arduino, and circuit design, I enjoy exploring how physics and coding intersect to build impactful systems.
+
+Beyond science, I’m a flutist and piccolo player in the University Brass Band - <span class="text-zinc-600">Rhythmic Energy</span>, where music sharpens my focus and creativity. As a leader and collaborator, I strive to bring clarity, innovation, and purpose to everything I do — blending logic with artistry to create meaningful impact.
     </p>
   </main>
 
