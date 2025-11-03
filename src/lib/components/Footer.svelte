c3c8b9784ee014c3fb88e6e05ad83291fc17a82c
Puspa Kamal Rai <132550374+puspkml@users.noreply.github.com>
2025-11-03 20:10:33 +0530

    Update Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -91,14 +91,14 @@
 					© {year} Puspa Kamal Rai. All Rights Reserved.
 				</p>
 				<p class="text-xs text-gray-500">
-					<li>
+					
 							<a
-								href={https://saichits.immadisairaj.dev/}
+								href="https://saichits.immadisairaj.dev/"
 								class="text-sm text-gray-300 hover:text-white transition-colors duration-200 hover:translate-x-1 inline-block"
 							>
 					With Blessings of <span class="text-red-400">Bhagavan Sri Sathya Sai Baba</span>
 						</a>
-					</li>
+					
 				</p>
 			</div>
 		</div>
